c0e3fb30f421c3a9bfa28e1b5d3f7d31d9837b25
Hsiungfei <hsiungfei@outlook.com>
2022-09-04 12:30:12 +0800

    Adding a Service to an ASP.NET Core Website | ASP.NET Core 101 [4 of 13]

--- ContosoCrafts.WebSite/Models/Product.cs
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -4,13 +4,13 @@ namespace ContosoCrafts.WebSite.Models
 {
     public class Product
     {
-        public string Id { get; set; }
-        public string Maker { get; set; }
-        public string Img { get; set; }
-        public string Url { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int[] Ratings { get; set; }
+        public string? Id { get; set; }
+        public string? Maker { get; set; }
+        public string? Img { get; set; }
+        public string? Url { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int[]? Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
 
---
+++ ContosoCrafts.WebSite/Services/JsonFileProductService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            this.WebHostEnvironment = webHostEnvironment;
+        }
+
+        public Product[]? GetProducts()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+            var res = JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+            return res;
+        }
+    }
+}
